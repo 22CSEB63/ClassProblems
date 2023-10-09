14620a17467968a403dc7060bbdce2e0729ca750
22CSEB63 <121368325+22CSEB63@users.noreply.github.com>
2023-10-09 06:17:02 +0530

    Add files via upload

---
+++ Main (5).java
@@ -0,0 +1,53 @@
+
+//Thread run and start method
+package com.mycompany.threaddemo;
+public class Threaddemo {
+
+    public static void main(String[] args) {
+        B b = new B();
+        A a = new A();
+        Thread Thread1= new Thread(b);
+        Thread Thread2 = new Thread(a);
+        try
+        {
+            Thread1.sleep(1000);
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Error");
+        }
+        Thread1.start();
+        Thread2.start();
+        
+        try
+        {
+            Thread2.sleep(1000);
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Error");
+        }
+        System.out.println("Hi everyone");
+    }
+}    
+class A implements Runnable
+{
+    public void run()
+    {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("Hi");
+        }
+    }
+}
+class B implements Runnable
+{
+    public void run()
+    {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+}
+*/
